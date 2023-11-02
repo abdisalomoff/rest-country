f5436a241c74f1ad6318855359574e2fab5f91cc
abdisalomoff <Avazbek6757@gmail.com>
2023-11-02 09:48:42 +0500

    add (search and redner)

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function renderCountries(country) {
     const cardCurrency = document.createElement('p');
     cardCurrency.textContent = `Currency: ${Object.keys(country.currencies)[0]}`;
 
-    // console.log(country);    
+    console.log(country);    
     // console.log(country.name.common);
     // console.log(country.population.toString().replace(","));
     // console.log(Object.keys(country.currencies)[0]);
@@ -51,7 +51,7 @@ countries.then((response) => response.json())
         data.forEach(country => {
             renderCountries(country)
         });
-    }).catch((error) => (error))
+    }).catch((error) => (error));
 
 // SEARCH COUNTRY ********
 
